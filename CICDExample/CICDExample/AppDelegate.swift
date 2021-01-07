e49f1b65e604feb73be203444dd50be9716f5eb3
Tankar Shah <tankar@promactinfo.com>
2021-01-07 16:52:20 +0530

    First commit

---
+++ CICDExample/CICDExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CICDExample
+//
+//  Created by Tankar Shah on 07/01/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ CICDExample/CICDExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CICDExample
+//
+//  Created by Tankar Shah on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
